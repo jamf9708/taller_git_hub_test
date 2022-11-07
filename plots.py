a6e9bc12120475da3f0339c995e36269f334f7a2
jamf9708 <jamf1997@hotmail.com>
2022-11-07 23:26:52 +0100

    adding code 2 dv2

--- plots.py
+++ plots.py
@@ -13,3 +13,37 @@ def read_dataset():
     global data
     my_dataset = './data/iris.csv'
     data = explore_data(my_dataset)
+    
+def plots():
+    global data
+    my_dataset = './data/iris.csv'
+    data = explore_data(my_dataset)
+    #Plot
+    if st.checkbox("Show Bar Plot with Matplotlib"):
+        st.write(data.plot.bar() )
+        st.pyplot()
+        
+    #Correlation
+    if st.checkbox("Show Correlation Matrix with Matplotlib"):
+        plt.matshow(data.corr())
+        st.pyplot()
+        
+    #Correlation
+    if st.checkbox("Show Correlation Matrix with Seaborn"):
+        st.write(sns.heatmap(data.corr()))
+        st.pyplot()
+
+def groupbys():    
+    v_group = data.groupby("Species").sum()   
+    # Group
+    if st.checkbox("Show Bar Chart Plot"):   
+        st.bar_chart(v_group)
+        
+    # Group
+    if st.checkbox("Show Line Chart Plot"):
+        st.line_chart(data)
+        
+    # Group
+    if st.checkbox("Show Area Chart Plot"):
+        st.area_chart(v_group)
+
