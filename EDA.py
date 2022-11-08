047e273388bac0209440290999dfc229ad17d5aa
jamf9708 <jamf1997@hotmail.com>
2022-11-08 01:39:18 +0100

    mmmamama

--- EDA.py
+++ EDA.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os 
 
 # EDA
-#Funt. to Load Dataset
+#Funt. to Load Datasettttttttttttttttjjjj
 @st.cache(persist=True)
 def explore_data(dataset):
     df = pd.read_csv(os.path.join(dataset), index_col=0)
