2ea8ab88a5ae7d1d301e5e228682a7ddf8addb3f
jamf9708 <jamf1997@hotmail.com>
2022-11-08 01:32:20 +0100

    prueba

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-#Funt. to Load Dataset
+#Funt. to Load Datasettttttttttttttttttttttttttttttttt
 @st.cache(persist=True)
 def explore_data(dataset):
     df = pd.read_csv(os.path.join(dataset), index_col=0)
@@ -13,3 +13,4 @@ def read_dataset():
     global data
     my_dataset = './data/iris.csv'
     data = explore_data(my_dataset)
+    
